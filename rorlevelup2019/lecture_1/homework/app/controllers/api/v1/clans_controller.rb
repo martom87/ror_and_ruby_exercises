bc2477696538d2f407b05d50128fc60e391ce0dd
martom87 <marcintomala87@gmail.com>
2019-06-05 23:54:10 +0200

    working create for samurai and clan

--- rorlevelup2019/lecture_1/homework/app/controllers/api/v1/clans_controller.rb
+++ rorlevelup2019/lecture_1/homework/app/controllers/api/v1/clans_controller.rb
@@ -1,10 +1,27 @@
 module Api
   module V1
     class ClansController < ApplicationController
+      skip_before_action :verify_authenticity_token
       def index
         clans = Clan.all
         render json: clans.to_json(only: %w[id name])
       end
+
+      def create
+        clan = Clan.new(clan_params)
+        if clan.save
+          render json: clan.to_json(only: %i[id name]), status: 201
+        else
+          render json: {error: clan.errors.full_messages}, status: 422
+        end
+      end
+
+      private
+
+      def clan_params
+        params.require(:clan).permit(:name)
+      end
+
     end
   end
 end
--- rorlevelup2019/lecture_1/homework/app/controllers/api/v1/samurais_controller.rb
+++ rorlevelup2019/lecture_1/homework/app/controllers/api/v1/samurais_controller.rb
@@ -1,7 +1,17 @@
 module Api
   module V1
     class SamuraisController < ApplicationController
-      before_action :set_clan, only: %i[index]
+      skip_before_action :verify_authenticity_token
+      before_action :set_clan, only: %i[index create]
+
+      def create
+        samurai = @clan.samurais.new(samurai_params)
+        if samurai.save
+          render json: samurai.to_json(only: SAMURAI_DATA), status: 201
+        else
+          render json: {errors: samurai.errors.full_messages}, status: 422
+        end
+      end
 
       def index
         @samurais = Samurai.all
@@ -10,9 +20,17 @@ module Api
 
       private
 
-      def set_clan
-        @clan = Clan.find(params[:clan_id])
+      SAMURAI_DATA = %i[name armor battles_number enrolled died].freeze
+
+      def samurai_params
+        params.require(:samurai).permit(SAMURAI_DATA)
       end
+
+      def set_clan
+        @clan ||= Clan.find(params[:clan_id])
+      end
+
+
     end
   end
 end
